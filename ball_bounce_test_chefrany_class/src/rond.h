110eb71752922a252a408b9206a45877d7957b28
chefrany <chefrany@gmail.com>
2015-10-06 17:36:16 +0200

    update_class test & ArrayList_vs_Vector

---
+++ ball_bounce_test_chefrany_class/src/rond.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "ofMain.h"
+
+class rond
+{
+public:
+	
+	
+
+	void setup();
+	void update();
+	void draw();
+	 // variables
+    float x;
+    float y;
+    float speedY;
+    float speedX;
+    int radius;
+    ofColor color;
+
+
+};
+
